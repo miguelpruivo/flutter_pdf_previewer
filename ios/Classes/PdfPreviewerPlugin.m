39d016370aa4ef25b77433c588d0f8e53a2c0df6
Miguel Ruivo <miguelpruivo@hotmail.com>
2018-10-24 16:45:26 +0100

    adds documentation

--- ios/Classes/PdfPreviewerPlugin.m
+++ ios/Classes/PdfPreviewerPlugin.m
@@ -1,5 +1,9 @@
 #import "PdfPreviewerPlugin.h"
 
+static NSString* const kDirectory = @"PDFPreviewer";
+static NSString* const kOutputBaseName = @"page";
+static NSString* const kFilePath = @"file:///";
+
 @implementation PdfPreviewerPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
   FlutterMethodChannel* channel = [FlutterMethodChannel
@@ -7,14 +11,33 @@
             binaryMessenger:[registrar messenger]];
   PdfPreviewerPlugin* instance = [[PdfPreviewerPlugin alloc] init];
   [registrar addMethodCallDelegate:instance channel:channel];
+ [PdfPreviewerPlugin clearCache];
+  
 }
+
++ (void)clearCache{
+    
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths objectAtIndex:0];
+    NSString *filePathAndDirectory = [documentsDirectory stringByAppendingPathComponent:kDirectory];
+    NSError *error;
+    
+    if (![[NSFileManager defaultManager] removeItemAtPath:filePathAndDirectory error:&error]){
+        NSLog(@"Remove directory error: %@", error);
+    }else{
+        NSLog(@"Cache removed with success.");
+    }
+    
+}
+    
+    
+
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
   if ([@"getPagePreview" isEqualToString:call.method]) {
       
       size_t pageNumber = (size_t)[call.arguments[@"pageNumber"] intValue];
       NSString * filePath = call.arguments[@"filePath"];
-      
       
       result([self getPDFPreview:filePath ofPage:pageNumber isTheLastPage: NO]);
   } else if([@"getLastPagePreview" isEqualToString:call.method]){
@@ -30,13 +53,11 @@
 -(NSString*)getPDFPreview:(NSString *)url ofPage:(size_t)pageNumber isTheLastPage:(BOOL)isOnLastPage
 {
     NSURL * sourcePDFUrl;
-    NSString * directory = @"PDFPreviewer";
-    NSString * outputBaseName = @"page";
     
-    if([url containsString:@"file:///"]){
+    if([url containsString:kFilePath]){
         sourcePDFUrl = [NSURL URLWithString:url];
     }else{
-        sourcePDFUrl = [NSURL URLWithString:[@"file:///" stringByAppendingString:url]];
+        sourcePDFUrl = [NSURL URLWithString:[kFilePath stringByAppendingString:url]];
     }
     
     
@@ -44,8 +65,9 @@
     size_t numberOfPages = CGPDFDocumentGetNumberOfPages(SourcePDFDocument);
     NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
     NSString *documentsDirectory = [paths objectAtIndex:0];
-    NSString *filePathAndDirectory = [documentsDirectory stringByAppendingPathComponent:directory];
+    NSString *filePathAndDirectory = [documentsDirectory stringByAppendingPathComponent:kDirectory];
     NSError *error;
+    
     
     if (![[NSFileManager defaultManager] createDirectoryAtPath:filePathAndDirectory
                                    withIntermediateDirectories:YES
@@ -64,7 +86,7 @@
     CGPDFPageRef SourcePDFPage = CGPDFDocumentGetPage(SourcePDFDocument, pageNumber);
     // CoreGraphics: MUST retain the Page-Refernce manually
     CGPDFPageRetain(SourcePDFPage);
-    NSString *relativeOutputFilePath = [NSString stringWithFormat:@"%@/%@%d.png", directory, outputBaseName, (int)pageNumber];
+    NSString *relativeOutputFilePath = [NSString stringWithFormat:@"%@/%@%d.png", kDirectory, kOutputBaseName, (int)pageNumber];
     NSString *ImageFileName = [documentsDirectory stringByAppendingPathComponent:relativeOutputFilePath];
     CGRect sourceRect = CGPDFPageGetBoxRect(SourcePDFPage, kCGPDFMediaBox);
     UIGraphicsBeginPDFContextToFile(ImageFileName, sourceRect, nil);
--- lib/pdf_previewer.dart
+++ lib/pdf_previewer.dart
@@ -6,13 +6,19 @@ import 'package:meta/meta.dart';
 class PdfPreviewer {
   static const MethodChannel _channel = const MethodChannel('pdf_previewer');
 
+  /// Creates a temporary PNG image for the provided [filePath]
+  ///
+  /// [pageNumber] defaults to `1` and must be equal or above it.
   static Future<dynamic> getPagePreview({@required String filePath, int pageNumber = 1}) async {
-    assert(pageNumber > 0);
+    assert(pageNumber > 0 && filePath != null);
     return _channel.invokeMethod('getPagePreview', {'filePath': filePath, 'pageNumber': pageNumber});
   }
 
-  static Future<dynamic> getLastPagePreview({@required String filePath}) async {
-    assert(pageNumber > 0);
+  /// Creates a temporary PNG image for the last page of the provided PDF in [filePath]
+  ///
+  /// If the file has only a single page, it will return it.
+  static Future<dynamic> getLastPagePreview({@required String filePath}) {
+    assert(filePath != null);
     return _channel.invokeMethod('getLastPagePreview', {'filePath': filePath});
   }
 }
